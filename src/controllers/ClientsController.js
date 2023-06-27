ad3c87a94fca120c25e75045ee699e7cfb572936
Lihsolive <liliamoliveira13@gmail.com>
2023-06-27 16:36:04 -0300

    Corrigindo falhas do POST e PUT

--- src/controllers/ClientsController.js
+++ src/controllers/ClientsController.js
@@ -1,72 +1,68 @@
-const database = require('../models')
+const database = require("../models");
 
 class ClientController {
-    // Get
-    static async getAllClients(req, res) {
-        try {
-            const allClients = await database.Clients.findAll({
-                attributes: ['id', 'name', 'email'] }); 
-            return res.status(200).json(allClients)
-        } catch (error) {
-            return res.status(500).json(error.message)
-        }
+  // Get
+  static async getAllClients(req, res) {
+    try {
+      const allClients = await database.Clients.findAll({
+        attributes: ["id", "name", "email"],
+      });
+      return res.status(200).json(allClients);
+    } catch (error) {
+      return res.status(500).json(error.message);
     }
-    // Get By ID
-    static async getOneClient(req, res) {
-        const { id } = req.params
-        try {
-            const oneClient = await database.Clients.findOne({
-                attributes: { exclude: ['password']}, 
-                where: { id: Number(id) } })
-            return res.status(200).json(oneClient)
-        } catch (error) {
-            return res.status(500).json(error.message)
-        }
+  }
+  // Get By ID
+  static async getOneClient(req, res) {
+    const { id } = req.params;
+    try {
+      const oneClient = await database.Clients.findOne({
+        attributes: { exclude: ["password"] },
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(oneClient);
+    } catch (error) {
+      return res.status(500).json(error.message);
     }
-    
-    // Post
-    static async createClient(req, res) {
-        const { name, nickname, email, phone, companyThatWorks, password } = req.body;
-        const client = { name, nickname, email, phone, companyThatWorks, password };
-        // const client = req.body;
-        try {
-          const createdClient = await database.Clients.create(client);
-          return res.status(200).json(createdClient);
-        } catch (error) {
-          return res.status(500).json(error.message);
-        }
-      }
+  }
 
-    // Put
-      static async updateClient(req, res) {
-        const { id } = req.params;
-        const { name, nickname, email, phone, companyThatWorks, password } = req.body;
-        const newClient = { name, nickname, email, phone, companyThatWorks, password };
-        try {
-          await database.Clients.update(newClient, { where: { id: Number(id) } });
-          const updatedClient = await database.Clients.findOne({
-            where: { id: Number(id) },
-          });
-          return res.status(200).json(updatedClient);
-        } catch (error) {
-          return res.status(500).json(error.messsage);
-        }
-      }
-
-    // Delete
-      static async deleteClient(req, res) {
-        const { id } = req.params;
-        try {
-          await database.Clients.destroy({ where: { id: Number(id) } });
-          return res.status(200).json({ message: `Id ${id} deleted!` });
-        } catch (error) {
-          return res.status(500).json(error.message);
-        }
-      }
+  // Post
+  static async createClient(req, res) {
+    const newClient = req.body;
+    try {
+      const newClientCreate = await database.Clients.create(newClient);
+      return res.status(200).json(newClientCreate);
+    } catch (error) {
+      return res.status(500).json(error.message);
     }
+  }
 
-module.exports = ClientController
+  // Put
+  static async updateClient(req, res) {
+    const { id } = req.params;
+    const newInfo = req.body;
+    try {
+      await database.Clients.update(newInfo, { where: { id: Number(id) } });
+      const clientUpdate = await database.Clients.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(clientUpdate);
+    } catch (error) {
+        console.log(req.body);
+        return res.status(500).json({ message: error.message, errors: error.errors });
+    }
+  }
 
+  // Delete
+  static async deleteClient(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Clients.destroy({ where: { id: Number(id) } });
+      return res.status(200).json({ message: `Id ${id} deletado!` });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+}
 
-
-    
+module.exports = ClientController;
--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const port = parseInt(process.env.NODE_PORT, 10) || 3000;
 
 const app = express();
 
+// para usar o middleware express.json(). Sem ele, as requisições de post e put (que usam json) não funcionarão corretamente.
+app.use(express.json());
+
 // routes
 app.use(routes);
 
